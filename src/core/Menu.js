bfe320f0916e7969d0bcc9e127e054420e26834a
Siddhant Bohra <abhisidds@gmail.com>
2019-08-18 14:32:13 +0530

    Updated Profile Page

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -57,8 +57,8 @@ const Menu = ({ history }) => (
                         <a className="nav-link" style={(isActive(history, "/signup"), { cursor: "pointer", color: "#fff" })} onClick={() => signout(() => history.push("/"))} >Sign Out</a>
                     </li>
                     <li className="nav-item">
-                        <Link to={`/user/${isAuthenticated().user._id}`}>
-                            <a className="nav-link">{`${isAuthenticated().user.name}`}</a>
+                        <Link className = "nav-link" to={`/user/${isAuthenticated().user._id}`} style={isActive(history, `/user/${isAuthenticated().user._id}`,{})}>
+                           {`${isAuthenticated().user.name}`}
                         </Link>
                     </li>
                 </>
--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { Redirect, Link } from 'react-router-dom'
 export const isAuthenticated = () => {
     if (typeof (window) == "undefined") {
         return false
@@ -17,40 +17,50 @@ class Profile extends Component {
         user: "",
         "redirectToSignin": false
     }
- 
+
     componentDidMount() {
         const userId = this.props.match.params.userId;
-        fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`,{
-            method : "GET",
-            headers : {
-                Accept : "application/json",
-                "Content-Type" : "application/json",
-                Authorization :   `Bearer ${isAuthenticated().token}`
+        fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${isAuthenticated().token}`
             }
-        }).then(response =>{
-           return response.json()
+        }).then(response => {
+            return response.json()
         })
-        .then(data =>{
-            console.log("data",data)
-            if(data.error)
-            {
-                console.log("ERROR")
-            }
-            else{
-                this.setState({
-                    user : data
-                })
-            }
-        })
+            .then(data => {
+                console.log("data", data)
+                if (data.error) {
+                    console.log("ERROR")
+                }
+                else {
+                    this.setState({
+                        user: data
+                    })
+                    console.log("State", isAuthenticated().user)
+                }
+            })
     }
 
     render() {
         return (
             <div className="container">
-                <h2 className="mt-5 mb-5">Profile</h2>
-                <p>Hello {isAuthenticated().user.name}</p>
-                <p>Email : {isAuthenticated().user.email}</p>
-                <p>{`Joined : ${new Date(this.state.user.created).toDateString()}`}</p>
+                <div className="col-md-6">
+                    <h2 className="mt-5 mb-5">Profile</h2>
+                    <p>Hello {isAuthenticated().user.name}</p>
+                    <p>Email : {isAuthenticated().user.email}</p>
+                    <p>{`Joined : ${new Date(this.state.user.created).toDateString()}`}</p>
+                </div>
+                <div>
+                    {isAuthenticated().user && isAuthenticated().user._id === this.state.user._id && (
+                        <div className="d-inline-block mt-5">
+                            <Link className="btn btn-raised btn-success mr-5" to={`/user/edit/${this.state.user._id}`}>Edit Profile</Link>
+                            <button className= "btn btn-raised btn-danger">Delete Profile</button>
+                        </div>
+                    )}
+                </div>
             </div>
         )
     }
